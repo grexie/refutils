18e06dead5b16224d38d380c5f3a366af42b78b0
Tim Behrsin <tim@behrsin.com>
2018-12-30 20:20:26 +0000

    docs

---
+++ package.go
@@ -0,0 +1,3 @@
+// Package refutils provides a collection of utility interfaces for use with referencing of objects.
+// Used throughout [go-v8](https://github.com/behrsin/go-v8) for reference tracking and reference-based mutex locking.
+package refutils
